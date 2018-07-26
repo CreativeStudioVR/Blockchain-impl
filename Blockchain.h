d324edadcb4ab620d2f2aa716aa31211464a2c46
Liu Jiacheng <liujch1998@gmail.com>
2018-07-25 22:50:15 -0500

    Backup

--- Blockchain.h
+++ Blockchain.h
@@ -9,5 +9,5 @@ struct Blockchain {
 	static std::map<Hash, Block> blocks;  // mapping from block header hash to block
 	static std::map<Hash, uint32_t> heights;  // mapping from block header hash to height; 0 for genesis block
 	static std::map<Address, uint32_t> balance;  // balance of each address
-	static Hash tail_block;  // hash of block with largest height
+	static Hash tail_block_hash;  // hash of block with largest height
 };
